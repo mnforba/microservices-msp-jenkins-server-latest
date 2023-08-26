73bde2c7750092babab2e44a10fb32dcc7d595aa
mnforba <nforbamel@gmail.com>
2023-08-26 14:49:49 -0400

    initial commit

---
+++ jenkins_variables.tf
@@ -0,0 +1,10 @@
+variable "vprof" {}
+variable "ami" {
+  description = "amazon linux 2 ami"
+}
+variable "region" {}
+variable "instance_type" {}
+variable "jenkins_server_secgr" {}
+variable "jenkins-server-tag" {}
+variable "jenkins-profile" {}
+variable "jenkins-role" {}
